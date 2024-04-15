97512980f08315a77799245cefe17cabfe839486
dosidat1503 <dosidat15031712@gmail.com>
2024-04-15 08:58:31 +0700

    thay đổi để có thể truy cập route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/test2', function () {
     return view('test2');
 });
+Route::get('getInfoAtStartLoadingHome', [HomeController::class, 'getInfoAtStartLoadingHome']);
+
