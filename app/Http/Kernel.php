15903ba05ef2f96157c55051a6c5a31ff2e3cada
dosidat1503 <dosidat15031712@gmail.com>
2025-02-17 20:45:03 +0700

    fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
+            \Illuminate\Http\Middleware\HandleCors::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, 
             // \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         ],
